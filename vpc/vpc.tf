49252d020849056aeeae0c7b17e3e427a8b41bd7
madhu <madhukarreddy879@gmail.com>
2023-11-21 11:44:21 +0530

    first commit

---
+++ vpc/vpc.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_vpc" "dev" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "dev-vpc"
+  }
+
+}
+
+resource "aws_subnet" "dev_public" {
+  vpc_id            = "aws_vpc.dev_public.id"
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1"
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_instance" "dev_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.dev_public.id
+
+  tags = {
+    Name = "dev"
+  }
+
+}
+
+
+###first

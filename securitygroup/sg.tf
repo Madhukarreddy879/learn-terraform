166c761b7e317f73115de172c5485825336f3db4
madhu <madhukarreddy879@gmail.com>
2023-11-21 13:48:01 +0530

    provider block

--- securitygroup/sg.tf
+++ securitygroup/sg.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  
+}
+
 resource "aws_vpc" "dev" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true

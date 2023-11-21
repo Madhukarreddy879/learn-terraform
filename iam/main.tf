bd0d3be35fed3866c56bceb4b7fccc7d23b65b6a
madhu <madhukarreddy879@gmail.com>
2023-11-21 21:19:04 +0530

    kgk

--- iam/main.tf
+++ iam/main.tf
@@ -32,7 +32,4 @@ output "access_key_id" {
   value = aws_iam_access_key.lucy_access_key.id
 }
 
-output "secret_access_key" {
-  value = aws_iam_access_key.lucy_access_key.secret
-  sensitive = = "true"
-}
+

d4e5ecb0018492a2336f879d91e8e1dba6130079
madhu <madhukarreddy879@gmail.com>
2023-11-23 11:01:25 +0530

    f

--- vpc1/main.tf
+++ vpc1/main.tf
@@ -59,4 +59,19 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
+resource "aws_route_table" "second_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  route{
+    cidr_block = "0.0.0.0/0"
+    gateway_id = ws_internet_gateway.gw.id
+
+    }
+
+    tags =  {
+       Name = "2nd route table"
+
+    }
+  }
+}
 

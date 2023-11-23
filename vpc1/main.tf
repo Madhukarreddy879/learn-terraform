0c31643d8b9b915ab2cafcb5d50940aa79295f07
madhu <madhukarreddy879@gmail.com>
2023-11-23 11:05:11 +0530

    ll

--- vpc1/main.tf
+++ vpc1/main.tf
@@ -64,7 +64,7 @@ resource "aws_route_table" "second_rt" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw
+    gateway_id = aws_internet_gateway.gw.id
 
     }
 

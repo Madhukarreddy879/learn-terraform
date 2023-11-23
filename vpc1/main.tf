55504b573bdcb271fbdcc81e883177286c004de0
madhu <madhukarreddy879@gmail.com>
2023-11-23 09:49:34 +0530

    kkj

--- vpc1/main.tf
+++ vpc1/main.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  
+}
+
+
+
 resource "aws_vpc" "vpc" {
     cidr_block = "10.0.0.0/16"
 

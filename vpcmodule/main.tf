e41ac69cae572e4b7ca317f9a0426a7fa68bc89f
madhu <madhukarreddy879@gmail.com>
2023-11-24 14:21:09 +0530

    fd
    addadlaajhflafaf
    kfskjfsk

---
+++ vpcmodule/main.tf
@@ -0,0 +1,20 @@
+resource "aws_vpc" "vpc_parameter" {
+  
+}
+
+resource "aws_subnet" "subnet_parameter" {
+  
+}
+
+
+resource "aws_internet_gateway" "igw_parameter" {
+  
+}
+
+resource "aws_route_table" "rt_parameterme" {
+  
+}
+
+resource "aws_route_table_association" "rt_association_parameter" {
+  
+}

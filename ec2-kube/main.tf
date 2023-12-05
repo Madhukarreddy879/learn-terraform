e30cc2c674a86ecfa8a43bb09cdcf6b9c226d80d
madhu <madhukarreddy879@gmail.com>
2023-12-05 21:09:33 +0530

    j

--- ec2-kube/main.tf
+++ ec2-kube/main.tf
@@ -1,5 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
 provider "aws" {
-  region = var.aws_region
+  region  = "us-east-1"
+  
 }
 
 resource "aws_instance" "ec2_instance" {
--- ec2-kube/variables.tf
+++ ec2-kube/variables.tf
@@ -1,7 +1,4 @@
-variable "aws_region" {
-  description = "AWS region"
-  default     = "us-east-1"
-}
+
 
 variable "ami_name" {
   description = "AMI name"
